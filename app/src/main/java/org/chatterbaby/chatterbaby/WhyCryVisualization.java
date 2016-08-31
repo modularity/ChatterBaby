13a18911468596715cde9f8455b2aea2d223db3a
modularity <laurenwdunlap@ucla.edu>
2016-08-31 16:49:01 -0700

    Final submission update

--- app/src/main/java/org/chatterbaby/chatterbaby/WhyCryVisualization.java
+++ app/src/main/java/org/chatterbaby/chatterbaby/WhyCryVisualization.java
@@ -2,8 +2,18 @@ package org.chatterbaby.chatterbaby;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 
 public class WhyCryVisualization extends Activity {
@@ -13,6 +23,23 @@ public class WhyCryVisualization extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_why_cry_visualization);
 
+      /*
+        // retrieve image path from algorithm response
+        Intent intent = getIntent();
+        String jpgPath = intent.getStringExtra("imageFile");
+        System.out.println(jpgPath);
+
+        // display image result
+        ImageView imgView= (ImageView) findViewById(R.id.imageView);
+
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 2;
+        Bitmap bm = BitmapFactory.decodeFile(jpgPath, options);
+        imgView.setImageBitmap(bm);
+        //imgView.setImageDrawable(Drawable.createFromPath(jpgPath));
+        //imgView.setVisibility(View.VISIBLE);
+        */
     }
 
 }
