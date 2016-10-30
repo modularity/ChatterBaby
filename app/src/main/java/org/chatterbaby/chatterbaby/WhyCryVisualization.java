7156abfb7a1e74923ae2ec87a1e6d8a96da31819
modularity <laurenwdunlap@ucla.edu>
2016-10-30 13:01:32 -0700

    Implemented algorithm result visualization
    Parsed json response to scrape probabilites
    Utilized a library that generates an animated graph

--- app/src/main/java/org/chatterbaby/chatterbaby/WhyCryVisualization.java
+++ app/src/main/java/org/chatterbaby/chatterbaby/WhyCryVisualization.java
@@ -1,45 +1,127 @@
 package org.chatterbaby.chatterbaby;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Environment;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.util.Log;
 
-import java.io.File;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.*;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class WhyCryVisualization extends Activity {
+    Float painProb;
+    Float hungryProb;
+    Float fussyProb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_why_cry_visualization);
 
-      /*
-        // retrieve image path from algorithm response
-        Intent intent = getIntent();
-        String jpgPath = intent.getStringExtra("imageFile");
-        System.out.println(jpgPath);
+        // retrieve json from algorithm response
+        String jsonStr = getIntent().getStringExtra("json");
+        System.out.println("Received intent: " + jsonStr);
+        parseJson(jsonStr);
 
-        // display image result
-        ImageView imgView= (ImageView) findViewById(R.id.imageView);
+        // a BarChart is initialized from xml
+        BarChart chart = (BarChart) findViewById(R.id.chart);
 
+        // configure xaxis (states)
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setTextSize(16f);
+        xAxis.setTypeface(Typeface.DEFAULT_BOLD);
+        xAxis.setValueFormatter(formatter);
+        xAxis.setDrawGridLines(false);
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 2;
-        Bitmap bm = BitmapFactory.decodeFile(jpgPath, options);
-        imgView.setImageBitmap(bm);
-        //imgView.setImageDrawable(Drawable.createFromPath(jpgPath));
-        //imgView.setVisibility(View.VISIBLE);
-        */
+        // configure yaxis (probabilities)
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        yAxis.setAxisMinValue(0f);
+        yAxis.setAxisMaxValue(1f);
+        yAxis.setDrawGridLines(false);
+
+        // set data within chart
+        BarData data = new BarData(getDataSet());
+        data.setBarWidth(0.8f); // set custom bar width
+        chart.setData(data);
+        chart.getAxisRight().setDrawLabels(false);
+        chart.getAxisRight().setDrawGridLines(false);
+        chart.setDescription("Predictive model of why baby is crying");
+        chart.setDescriptionPosition(0,0);
+        chart.animateY(2400);
+        chart.setFitBars(true); // make the x-axis fit exactly all bars
+        //chart.setTouchEnabled(false);
+        //chart.setDrawGridBackground(false);
+
+        chart.setHardwareAccelerationEnabled(true);
+        chart.setDrawValueAboveBar(false);
+        chart.invalidate(); // refresh
+
+        Log.i("Visualization", "displayed image result");
     }
 
+    private void parseJson(String jsonStr) {
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                painProb = (Float) jsonObj.get("Pain");
+                hungryProb = (Float) jsonObj.get("Hungry");
+                fussyProb = (Float) jsonObj.get("Fussy");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private BarDataSet getDataSet() {
+
+        // create dataset
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, painProb));
+        entries.add(new BarEntry(1f, hungryProb));
+        entries.add(new BarEntry(2f, fussyProb));
+
+        BarDataSet barDataSet = new BarDataSet(entries, "Probability of state");
+        //barDataSet.setValueFormatter(new PercentFormatter());
+        barDataSet.setColors(new int[]{Color.rgb(246, 18, 18), Color.rgb(0, 135, 0), Color.rgb(0, 102, 204)});
+        barDataSet.setValueTextSize(14f);
+        barDataSet.setValueTextColor(Color.rgb(255,255,255));
+        //barDataSet.setBarShadowColor(Color.rgb(215, 215, 215));
+
+        return barDataSet;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("Pain");
+        xAxis.add("Hungry");
+        xAxis.add("Fussy");
+        return xAxis;
+    }
+
+    // Formatter
+    final String[] values = new String[]{"Pain", "Hungry", "Fussy"};
+    AxisValueFormatter formatter = new AxisValueFormatter() {
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return values[(int) value];
+        }
+        // we don't draw numbers, so no decimal digits needed
+        @Override
+        public int getDecimalDigits() {  return 0; }
+    };
 }
